f3323edb025a32452b86c9b0f444186102349b3e
christ <zengguang_712@163.com>
2016-07-23 12:41:42 +0800

    23

---
+++ Leetcode/src/Stack/largest_rectangle_in_histogram.java
@@ -0,0 +1,12 @@
+package Stack;
+/** 
+ * author: christ 
+ * data：2016年7月22日 下午7:50:26 
+ * 类说明 
+ */
+public class largest_rectangle_in_histogram {
+	public int largestRectangleArea(int[] height){
+		return 0;
+	}
+	
+}
