b6d057d8ebe1fd829a288fcdc8c8a4c42294643a
christ <zengguang_712@163.com>
2016-07-23 15:31:53 +0800

    最大矩形面积

--- Leetcode/src/Stack/largest_rectangle_in_histogram.java
+++ Leetcode/src/Stack/largest_rectangle_in_histogram.java
@@ -1,4 +1,7 @@
 package Stack;
+
+import java.util.Stack;
+
 /** 
  * author: christ 
  * data：2016年7月22日 下午7:50:26 
@@ -6,7 +9,24 @@ package Stack;
  */
 public class largest_rectangle_in_histogram {
 	public int largestRectangleArea(int[] height){
-		return 0;
+		Stack<Integer> S = new Stack<>();
+		int[] h = new int[height.length+1];
+		for(int i = 0; i < height.length; i++){
+			h[i] = height[i];
+		}
+		h[height.length] = 0;
+		int res = 0;
+		for(int i = 0; i < h.length; i++){
+			if(S.isEmpty() || h[i] > h[S.peek()]){
+				S.push(i);
+			}else{
+				int temp = S.peek();
+				S.pop();
+				res = Math.max(res, h[temp]*(S.isEmpty()?i:i-S.peek()-1));
+				i--;
+			}
+		}
+		return res;
 	}
 	
 }
